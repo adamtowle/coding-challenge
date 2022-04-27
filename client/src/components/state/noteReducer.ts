57833e8db94ae31e7777bab86be2cadaf22dd959
John Olegario <john.olegario@method4.co.uk>
2022-04-27 22:00:04 +0100

    Note UI toggle notes

--- client/src/components/state/noteReducer.ts
+++ client/src/components/state/noteReducer.ts
@@ -1,15 +1,19 @@
 import { createReducer, Reducer } from "@reduxjs/toolkit";
-import { setNotes } from "./noteActions";
+import { setNotes, setIsAll } from "./noteActions";
 import { BasicNotes } from "./notesState";
 
 export interface NotesActionsState {
     notes: BasicNotes[];
+    isAll: Boolean;
 }
 
-const initialState = { notes: []} as NotesActionsState;
+const initialState = { notes: [], isAll: false } as NotesActionsState;
 
 export const notesReducer: Reducer<NotesActionsState> = createReducer(initialState, (builder) => {
     builder.addCase(setNotes, (state, action) => {
         state.notes = action.payload;
+    })
+    builder.addCase(setIsAll, (state, action) => {
+        state.isAll = action.payload;
     })
 })
